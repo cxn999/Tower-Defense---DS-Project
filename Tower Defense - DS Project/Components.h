77cac20244b45d56a7d9c61e786f9614ce24125b
Alvaro <alvarolialeti@gmail.com>
2024-07-01 20:08:39 -0400

    vec2 scale from component eliminated

--- Tower Defense - DS Project/Components.h
+++ Tower Defense - DS Project/Components.h
@@ -15,13 +15,12 @@ class CTransform : public Component {
 public:
 	Vec2 pos = { 0.0, 0.0 };
 	Vec2 prevPos = { 0.0, 0.0 };
-	Vec2 scale = { 1.0, 1.0 };
 	Vec2 velocity = { 0.0, 0.0 };
 	float angle = 0;
 
 	CTransform() {}
 	CTransform(const Vec2 & p) : pos(p) {}
-	CTransform(const Vec2& p, const Vec2& v, const Vec2& sc, float a) : pos(p), velocity(v), scale(sc), angle(a) {}
+	CTransform(const Vec2& p, const Vec2& v, float a) : pos(p), velocity(v), angle(a) {}
 };
 
 class CLifeSpan : public Component {
